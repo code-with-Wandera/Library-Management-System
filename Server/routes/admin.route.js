0950b3557c6b1854db34a9ddc40bab44f325ee27
code-with-wandera <collinswandera560@gmail.com>
2026-02-14 00:19:18 +0300

    updated admin route

--- Server/routes/admin.route.js
+++ Server/routes/admin.route.js
@@ -3,7 +3,8 @@ import {
   getStats, 
   getRecentBorrows, 
   getTopBorrowers, 
-  getMostBorrowedBooks 
+  getMostBorrowedBooks, 
+  getDashboardData
 } from "../controllers/admin.controller.js";
 import { verifyToken, adminOnly } from "../middlewares/auth.middleware.js";
 
@@ -22,5 +23,5 @@ router.get("/stats", getStats);
 router.get("/recent-borrows", getRecentBorrows);
 router.get("/top-borrowers", getTopBorrowers);
 router.get("/most-borrowed-books", getMostBorrowedBooks);
-
+router.get("/dashboard-data", getDashboardData);
 export default router;
